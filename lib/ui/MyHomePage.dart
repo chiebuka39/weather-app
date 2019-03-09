61b06225babb9162c3e63fb021377d37cb3df4e0
Anelechi chiebuka <anelechichiebuka@gmail.com>
2019-03-09 06:41:57 +0100

    add code

---
+++ lib/ui/MyHomePage.dart
@@ -0,0 +1,30 @@
+import 'package:chat/ui/Weather.dart';
+import 'package:flutter/material.dart';
+
+
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => new _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+
+  @override
+  Widget build(BuildContext context) {
+   
+    return new Scaffold(
+      backgroundColor: Colors.lightBlue,
+      appBar: new AppBar(
+        title: new Text(widget.title),
+      ),
+      body: Weather()
+      );
+  }
+}
